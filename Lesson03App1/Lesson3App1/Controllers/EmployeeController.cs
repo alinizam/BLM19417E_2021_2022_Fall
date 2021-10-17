291bb319075079e2adeca9d031fce2cf076341be
alinizam <alinizam99@gmail.com>
2021-10-17 08:32:03 +0300

    LEsson 3

---
+++ Lesson03App1/Lesson3App1/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using Lesson3App1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson3App1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _repo;
+
+        public EmployeeController(IEmployeeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+           
+            
+            return View(_repo.getEmployees());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            _repo.getEmployees().Add(e);
+            ViewData["EmpCount"] = _repo.getEmployees().Count();
+            ViewData["Empployees"] = _repo.getEmployees();
+            ViewBag.EmpCount = _repo.getEmployees().Count();
+            @TempData["EmpCount"] = _repo.getEmployees().Count();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Lesson03App1/Lesson3App1/Models/EmployeeMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson3App1.Models
+{
+    public class EmployeeMessage
+    {
+        Employee employeee;
+        string message;
+
+    }
+}
