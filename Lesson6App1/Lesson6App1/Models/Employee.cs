fa4b37ea0685cbee5ac95bc5f25b24956fafccd5
alinizam <alinizam99@gmail.com>
2021-11-03 14:47:06 +0300

    Lesson6

---
+++ Lesson6App1/Lesson6App1/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Lesson6App1.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? DepartmentId { get; set; }
+        public int Salary { get; set; }
+    }
+}
